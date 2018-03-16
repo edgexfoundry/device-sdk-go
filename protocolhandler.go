b7511a6c51d32a18115662ba4bcd4d236e04e51b
Tony Espy <espy@canonical.com>
2018-03-16 10:08:44 -0700

    [gxds] Use SPDX license
    Signed-off-by: Tony Espy <espy@canonical.com>

--- protocolhandler.go
+++ protocolhandler.go
@@ -1,24 +1,11 @@
 // -*- Mode: Go; indent-tabs-mode: t -*-
-
-/*
- * Copyright (C) 2017-2018 Canonical Ltd
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-// Package gxds defines interfaces used to build an EdgeX Foundry device
-// service.  These interaces provide and asbstraction layer for the device
+//
+// Copyright (C) 2017-2018 Canonical Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// This package defines an interface used to build an EdgeX Foundry device
+// service.  This interace provides an asbstraction layer for the device
 // or protocol specific logic of a device service.
 package gxds
 
@@ -28,13 +15,25 @@ import "github.com/edgexfoundry/core-domain-go/models"
 // by other components of an EdgeX device service to interact with
 // a specific class of devices.
 type ProtocolHandler interface {
+	// Initialize performs protocol-specific initialization
+	// for the device service.
 	Initialize()
+
+	// DisconnectDevice...
 	DisconnectDevice(device models.Device)
+
+	// InitializeDevice...
 	InitializeDevice(device models.Device)
+
+	// Scan
 	Scan()
 
+	// CommandExists...
 	// TODO: can CommandExists be handled by the devicestore?
 	CommandExists(device models.Device, command string) bool
+
+	// ExecuteCommand...
 	ExecuteCommand(device models.Device, command string, args string) map[string]string
+	// CompleteTransaction...
 	CompleteTransaction(transactionId string, opId string, readings []models.Reading)
 }
