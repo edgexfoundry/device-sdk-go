d8d40852a65582862725234cf0902111af7bfcd6
Tony Espy <espy@canonical.com>
2018-03-16 12:13:41 -0700

    [gxds] Add config file handling
    Signed-off-by: Tony Espy <espy@canonical.com>

---
+++ config.go
@@ -0,0 +1,63 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+//
+// Copyright (C) 2017-2018 Canonical Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+package gxds
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type ConfigFile struct {
+	ServiceName                  string
+	ServiceHost                  string
+	ServicePort                  int
+	Labels                       []string
+	Timeout                      int
+	OpenMessage                  string
+	ConnectRetries               int
+	ConnectWait                  int
+	ConnectInterval              int
+	MaxLimit                     int
+	HeartBeatTime                int
+	DataTransform                bool
+	MetadbHost                   string
+	MetadbPort                   int
+	CoreHost                     string
+	CorePort                     int
+	LoggingFile                  string
+	LoggingRemoteURL             string
+	DefaultScheduleName          string
+	DefaultScheduleFrequency     string
+	DefaultScheduleEventName     string
+	DefaultScheduleEventPath     string
+	DefaultScheduleEventService  string
+	DefaultScheduleEventSchedule string
+}
+
+func LoadConfig(configPath *string) (config ConfigFile, err error) {
+	config = ConfigFile{}
+	f, err := os.Open(*configPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "config file: %s; open failed: %v\n", *configPath, err)
+		return config, err
+	}
+	defer f.Close()
+
+	fmt.Fprintf(os.Stdout, "config file opened: %s\n", *configPath)
+
+	jsonParser := json.NewDecoder(f)
+	err = jsonParser.Decode(&config)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		return config, err
+	}
+
+	fmt.Fprintf(os.Stdout, "name: %v\n", config)
+
+	return config, nil
+}
