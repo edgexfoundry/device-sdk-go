7e9d1e3187990a04e093d50441efb2cd63c9aae5
Tony Espy <espy@canonical.com>
2018-03-16 10:34:11 -0700

    [daemon] Use SDK license
    Signed-off-by: Tony Espy <espy@canonical.com>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1,27 +1,15 @@
 // -*- Mode: Go; indent-tabs-mode: t -*-
-
-/*
- * Copyright (C) 2017 Canonical Ltd
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
+//
+// Copyright (C) 2017-2018 Canonical Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+//
 // Package daemon(service?) implements the core logic of a device service,
 // which include loading configuration, handling service registration,
 // creation of object caches, REST APIs, and basic daemon functionality.
 // Clients of this package must provide concrete implementations of the
 // device-specific interfaces (e.g. ProtocolHandler).
+//
 package daemon
 
 import (
