e4566de4e0080e66b96eac5ee088aee3b92d2b50
Tony Espy <espy@canonical.com>
2018-03-29 18:07:09 -0400

    [service] add TestDeviceLocked (partially disabled)
    The framework for this test is outlined, but in order
    to complete, the cache layer needs to be converted to
    use interfaces so the various caches can be mocked for
    unit testing.
    
    Signed-off-by: Tony Espy <espy@canonical.com>

--- service/command_test.go
+++ service/command_test.go
@@ -13,10 +13,15 @@ import (
 	"strings"
 	"testing"
 
+// TODO: imports commented out till cache objects become interfaces
+//	"time"
+
 	"bitbucket.org/tonyespy/gxds"
 	"bitbucket.org/tonyespy/gxds/cache"
 	logger "github.com/edgexfoundry/edgex-go/support/logging-client"
+//	"github.com/edgexfoundry/edgex-go/core/domain/models"
 	"github.com/gorilla/mux"
+//	"gopkg.in/mgo.v2/bson"
 )
 
 const deviceCommandTest = "device-command-test"
@@ -56,7 +61,6 @@ func TestCommandServiceLocked(t *testing.T) {
 func TestCommandNoDevice(t *testing.T) {
 	badDeviceId := "5abae51de23bf81c9ef0f390"
 
-	// TODO: add dummy Config
 	lc := logger.NewClient("command_test", false, "./command_test.log")
 
 	// Setup dummy service with logger, and mocked devices cache
@@ -82,4 +86,62 @@ func TestCommandNoDevice(t *testing.T) {
 	if body != expected {
 		t.Errorf("ServiceLocked: handler returned wrong body:\nexpected: %s\ngot:      %s", expected, body)
 	}
+}
+
+// TestCommandNoDevice tests the command REST call when the device specified
+// by deviceId is locked.
+func TestCommandDeviceLocked(t *testing.T) {
+//	testDeviceId := "5abae51de23bf81c9ef0f390"
+
+	lc := logger.NewClient("command_test", false, "./command_test.log")
+
+	// Setup dummy service with logger, and mocked devices cache
+	// Empty cache will by default have no devices.
+	s := &Service{lc: lc}
+	s.cd = cache.NewDevices(s.Config, nil)
+
+	/* TODO: adding a device to the devices cache requires a live metadata instance. We need
+	 * create interfaces for all of the caches, so that they can be mocked in unit tests.
+
+	millis := time.Now().UnixNano() * int64(time.Nanosecond) / int64(time.Microsecond)
+
+	// TODO: does HTTPMethod need to be specified?
+	addr = models.Addressable{
+		BaseObject: models.BaseObject{
+			Origin: millis,
+		},
+		Name:       s.Config.ServiceName,
+		HTTPMethod: "POST",
+		Protocol:   "HTTP",
+		Address:    "localhost",
+		Port:       "2112",
+		Path:       "/api/v1/callback",
+	}
+	addr.Origin = millis
+
+	// Create a locked Device
+	d := &models.Device{Name: "DummyDevice", AdminState: "LOCKED", OperatingState: "ENABLED"}
+	d.Id = bson.ObjectIdHex(testDeviceId)
+
+	s.cd.Add(d)
+
+	ch := &commandHandler{fn: commandFunc, s: s}
+	req := httptest.NewRequest("GET", fmt.Sprintf("%s/%s/%s", deviceV1, testDeviceId, testCmd), nil)
+	req = mux.SetURLVars(req, map[string]string{"deviceId": testDeviceId, "cmd": testCmd})
+
+	rr := httptest.NewRecorder()
+	ch.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusLocked {
+		t.Errorf("NoDevice: handler returned wrong status code: got %v want %v",
+			status, http.StatusLocked)
+	}
+
+	body := strings.TrimSpace(rr.Body.String())
+	expected := "device: " + testDeviceId + " locked; GET " + deviceV1 + "/" + testDeviceId + "/" + testCmd
+
+	if body != expected {
+		t.Errorf("DeviceLocked: handler returned wrong body:\nexpected: %s\ngot:      %s", expected, body)
+	}
+	*/
 }
