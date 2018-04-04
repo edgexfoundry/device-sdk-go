dd3811f35c6a378758ea6cc357727b638df943f2
Tony Espy <espy@canonical.com>
2018-04-04 19:43:28 -0400

    Fix log client syntax error in service.go
    Use fmt.Sprintf vs. fmt.Printf in log calls.
    
    Signed-off-by: Tony Espy <espy@canonical.com>

--- service/service.go
+++ service/service.go
@@ -18,7 +18,6 @@ import (
 
 	"fmt"
 	"net/http"
-	"os"
 	"strconv"
 	"time"
 
@@ -83,7 +82,7 @@ func (s *Service) attemptInit(done chan<- struct{}) {
 			s.Config.ServiceName, s.initAttempts))
 
 		// check for addressable
-		s.lc.Error(fmt.Fprintf(os.Stderr, "Trying to find addressable for: %s", s.Config.ServiceName))
+		s.lc.Error(fmt.Sprintf("Trying to find addressable for: %s", s.Config.ServiceName))
 		addr, err := s.ac.AddressableForName(s.Config.ServiceName)
 		if err != nil {
 			s.lc.Error(fmt.Sprintf("AddressableForName: %s; failed: %v", s.Config.ServiceName, err))
@@ -179,14 +178,14 @@ func (s *Service) attemptInit(done chan<- struct{}) {
 
 // Initialize the Service
 func (s *Service) Init(configFile *string, proto gxds.ProtocolDriver) (err error) {
-	s.lc.Debug(fmt.Fprintf(os.Stdout, "configuration file is: %s\n", *configFile))
-	s.lc.Debug(fmt.Fprintf(os.Stdout, "proto is: %v\n", proto))
+	s.lc.Debug(fmt.Sprintf("configuration file is: %s\n", *configFile))
+	s.lc.Debug(fmt.Sprintf("proto is: %v\n", proto))
 
 	// TODO: check if proto is nil, and fail...
 
 	s.Config, err = gxds.LoadConfig(configFile)
 	if err != nil {
-		s.lc.Error(fmt.Fprintf(os.Stderr, "error loading config file: %v\n", err))
+		s.lc.Error(fmt.Sprintf("error loading config file: %v\n", err))
 		return err
 	}
 
