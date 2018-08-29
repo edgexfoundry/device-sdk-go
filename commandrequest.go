06b333ba3f183e81215f797e19cd41aabe02dc13
Tony Espy <espy@canonical.com>
2018-08-29 13:25:05 -0400

    Re-factor ProtocolDriver HandleOperation function
    This change re-factors the logic for handling the command
    endpoint. In particular, instead of looping through the
    ResourceOperations of a particular call to the command endpoint,
    it now passes them all in a single call to the ProtocolDriver
    and waits for all the results to be returned as a single slice.
    This change also renames HandleOperation to HandleCommands.
    
    Signed-off-by: Tony Espy <espy@canonical.com>

---
+++ commandrequest.go
@@ -0,0 +1,18 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+//
+// Copyright (C) 2018 Canonical Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+package device
+
+import "github.com/edgexfoundry/edgex-go/pkg/models"
+
+type CommandRequest struct {
+	// RO is a ResourceOperation
+	RO models.ResourceOperation
+	// DeviceObject (aka device resource) represents the device resource
+	// to be read or set. It can be used to access the attributes map,
+	// PropertyValue, and PropertyUnit structs.
+	DeviceObject models.DeviceObject
+}
