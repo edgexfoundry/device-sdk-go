d3cfee1399309861e7515eb9a34ee8429befab89
Joan Duran <jduran@circutor.com>
2018-09-13 14:00:30 +0200

    Implemented callback to handle device adminState
    Signed-off-by: Joan Duran <jduran@circutor.com>

--- update.go
+++ update.go
@@ -11,31 +11,49 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
 
 	"github.com/edgexfoundry/edgex-go/pkg/models"
 )
 
 func callbackHandler(w http.ResponseWriter, req *http.Request) {
-	// use req.Method vs. method
-
+	defer req.Body.Close()
 	dec := json.NewDecoder(req.Body)
 	cbAlert := models.CallbackAlert{}
 
 	err := dec.Decode(&cbAlert)
 	if err != nil {
-		// TODO: handle error properly
-		fmt.Fprintf(os.Stderr, "service: callbackHandler invalid request: %v\n", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		svc.lc.Error(fmt.Sprintf("Invalid callback request: %v", err))
+		return
 	}
 
-	action := cbAlert.ActionType
-	id := cbAlert.Id
+	if (cbAlert.Id == "") || (cbAlert.ActionType == "") {
+		http.Error(w, "Missing parameters", http.StatusBadRequest)
+		svc.lc.Error(fmt.Sprintf("Missing callback parameters"))
+		return
+	}
 
-	fmt.Fprintf(os.Stderr, "service: callbackHandler action: %v id: %s\n", action, id)
+	// It was decided at the last F2F, that the one Core Metadata callback
+	// function to be supported for Dehli is handling changes to a device's
+	// adminState (LOCKED or UNLOCKED).
+	if (cbAlert.ActionType == models.DEVICE) && (req.Method == http.MethodPut) {
+		err = dc.UpdateAdminState(cbAlert.Id)
+		if err == nil {
+			svc.lc.Info(fmt.Sprintf("Updated device %s admin state", cbAlert.Id))
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			svc.lc.Error(fmt.Sprintf("Couldn't update device %s admin state: %v", cbAlert.Id, err.Error()))
+			return
+		}
+	} else {
+		svc.lc.Error(fmt.Sprintf("Invalid device method and/or action type: %s - %s", req.Method, cbAlert.ActionType))
+		http.Error(w, "Invalid device method and/or action type", http.StatusBadRequest)
+		return
+	}
 
 	io.WriteString(w, "OK")
 }
 
 func initUpdate() {
-	svc.r.HandleFunc("callback", callbackHandler)
+	svc.r.HandleFunc("/callback", callbackHandler)
 }
