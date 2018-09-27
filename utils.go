b68d692e579bceddfc003dec24556929f5406550
Tony Espy <espy@canonical.com>
2018-09-27 11:31:17 -0400

    Fix utils.go copyright file header
    Copyright headers should consist of single lines for each
    contributor of the form:
    
    // Copyright (C) <Year | Year range> <Contributor name>
    
    Signed-off-by: Tony Espy <espy@canonical.com>

--- utils.go
+++ utils.go
@@ -1,8 +1,7 @@
 // -*- Mode: Go; indent-tabs-mode: t -*-
 //
-// Copyright (C) 2017-2018
-// Canonical Ltd
-// IOTech
+// Copyright (C) 2017-2018 Canonical Ltd
+// Copyright (C) 2018 IOTech Ltd
 //
 // SPDX-License-Identifier: Apache-2.0
 
