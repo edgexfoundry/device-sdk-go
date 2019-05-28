c1ed8237e3023a7a0a2aa1e3172508adea2d8212
lenny-intel <leonard.goodell@intel.com>
2019-05-28 11:47:47 -0700

    Changed SDK to never exit so caller can do clean-up
    Signed-off-by: lenny-intel <leonard.goodell@intel.com>

--- examples/simple-filter-xml-post/main.go
+++ examples/simple-filter-xml-post/main.go
@@ -50,5 +50,12 @@ func main() {
 
 	// 5) Lastly, we'll go ahead and tell the SDK to "start" and begin listening for events
 	// to trigger the pipeline.
-	edgexSdk.MakeItRun()
-}
+	err := edgexSdk.MakeItRun()
+	if err != nil {
+		edgexSdk.LoggingClient.Error("MakeItRun returned error: ", err.Error())
+		os.Exit(-1)
+	}
+
+	// Do any required cleanup here
+
+	os.Exit(0)}
