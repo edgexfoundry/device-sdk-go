2f0167151fb1f522997b1cefd46d5b701c7a3602
Felix Ting <felix@iotechsys.com>
2019-09-03 20:06:53 +0800

    Make data for mock things readable and maintainable
    Signed-off-by: Felix Ting <felix@iotechsys.com>

---
+++ internal/mock/consts.go
@@ -0,0 +1,28 @@
+package mock
+
+const (
+	DeviceBool  = "Random-Boolean-Generator"
+	DeviceInt   = "Random-Integer-Generator"
+	DeviceUint  = "Random-UnsignedInteger-Generator"
+	DeviceFloat = "Random-Float-Generator"
+	DeviceNew   = "New-Device"
+
+	ProfileBool  = "Random-Boolean-Generator"
+	ProfileInt   = "Random-Integer-Generator"
+	ProfileUint  = "Random-UnsignedInteger-Generator"
+	ProfileFloat = "Random-Float-Generator"
+	ProfileNew   = "New-Device"
+
+	ResourceObjectBool        = "RandomValue_Bool"
+	ResourceObjectInt8        = "RandomValue_Int8"
+	ResourceObjectInt16       = "RandomValue_Int16"
+	ResourceObjectInt32       = "RandomValue_Int32"
+	ResourceObjectInt64       = "RandomValue_Int64"
+	ResourceObjectUint8       = "RandomValue_Uint8"
+	ResourceObjectUint16      = "RandomValue_Uint16"
+	ResourceObjectUint32      = "RandomValue_Uint32"
+	ResourceObjectUint64      = "RandomValue_Uint64"
+	ResourceObjectFloat32     = "RandomValue_Float32"
+	ResourceObjectFloat64     = "RandomValue_Float64"
+	ResourceObjectRandFloat32 = "randfloat32"
+)
