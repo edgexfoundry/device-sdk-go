ef8f9ac8a3f63b5a35a606854b560a6114236552
Chris Hung <chris@iotechsys.com>
2021-02-01 12:56:46 +0800

    refactor: update container package for v2
    Signed-off-by: Chris Hung <chris@iotechsys.com>

--- internal/container/client.go
+++ internal/container/client.go
@@ -1,6 +1,6 @@
 // -*- Mode: Go; indent-tabs-mode: t -*-
 //
-// Copyright (C) 2020 IOTech Ltd
+// Copyright (C) 2020-2021 IOTech Ltd
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -8,48 +8,31 @@ package container
 
 import (
 	"github.com/edgexfoundry/go-mod-bootstrap/v2/di"
-	"github.com/edgexfoundry/go-mod-core-contracts/v2/clients/coredata"
-	"github.com/edgexfoundry/go-mod-core-contracts/v2/clients/general"
-	"github.com/edgexfoundry/go-mod-core-contracts/v2/clients/metadata"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/v2/clients/interfaces"
 )
 
-var GeneralClientName = di.TypeInstanceToName((*general.GeneralClient)(nil))
-var MetadataDeviceClientName = di.TypeInstanceToName((*metadata.DeviceClient)(nil))
-var MetadataDeviceServiceClientName = di.TypeInstanceToName((*metadata.DeviceServiceClient)(nil))
-var MetadataDeviceProfileClientName = di.TypeInstanceToName((*metadata.DeviceProfileClient)(nil))
-var MetadataAddressableClientName = di.TypeInstanceToName((*metadata.AddressableClient)(nil))
-var MetadataProvisionWatcherClientName = di.TypeInstanceToName((*metadata.ProvisionWatcherClient)(nil))
-var CoredataEventClientName = di.TypeInstanceToName((*coredata.EventClient)(nil))
-var CoredataValueDescriptorClientName = di.TypeInstanceToName((*coredata.ValueDescriptorClient)(nil))
+var MetadataDeviceClientName = di.TypeInstanceToName((*interfaces.DeviceClient)(nil))
+var MetadataDeviceServiceClientName = di.TypeInstanceToName((*interfaces.DeviceServiceClient)(nil))
+var MetadataDeviceProfileClientName = di.TypeInstanceToName((*interfaces.DeviceProfileClient)(nil))
+var MetadataProvisionWatcherClientName = di.TypeInstanceToName((*interfaces.ProvisionWatcherClient)(nil))
+var CoredataEventClientName = di.TypeInstanceToName((*interfaces.EventClient)(nil))
 
-func GeneralClientFrom(get di.Get) general.GeneralClient {
-	return get(GeneralClientName).(general.GeneralClient)
+func MetadataDeviceClientFrom(get di.Get) interfaces.DeviceClient {
+	return get(MetadataDeviceClientName).(interfaces.DeviceClient)
 }
 
-func MetadataDeviceClientFrom(get di.Get) metadata.DeviceClient {
-	return get(MetadataDeviceClientName).(metadata.DeviceClient)
+func MetadataDeviceServiceClientFrom(get di.Get) interfaces.DeviceServiceClient {
+	return get(MetadataDeviceServiceClientName).(interfaces.DeviceServiceClient)
 }
 
-func MetadataDeviceServiceClientFrom(get di.Get) metadata.DeviceServiceClient {
-	return get(MetadataDeviceServiceClientName).(metadata.DeviceServiceClient)
+func MetadataDeviceProfileClientFrom(get di.Get) interfaces.DeviceProfileClient {
+	return get(MetadataDeviceProfileClientName).(interfaces.DeviceProfileClient)
 }
 
-func MetadataDeviceProfileClientFrom(get di.Get) metadata.DeviceProfileClient {
-	return get(MetadataDeviceProfileClientName).(metadata.DeviceProfileClient)
+func MetadataProvisionWatcherClientFrom(get di.Get) interfaces.ProvisionWatcherClient {
+	return get(MetadataProvisionWatcherClientName).(interfaces.ProvisionWatcherClient)
 }
 
-func MetadataAddressableClientFrom(get di.Get) metadata.AddressableClient {
-	return get(MetadataAddressableClientName).(metadata.AddressableClient)
-}
-
-func MetadataProvisionWatcherClientFrom(get di.Get) metadata.ProvisionWatcherClient {
-	return get(MetadataProvisionWatcherClientName).(metadata.ProvisionWatcherClient)
-}
-
-func CoredataEventClientFrom(get di.Get) coredata.EventClient {
-	return get(CoredataEventClientName).(coredata.EventClient)
-}
-
-func CoredataValueDescriptorClientFrom(get di.Get) coredata.ValueDescriptorClient {
-	return get(CoredataValueDescriptorClientName).(coredata.ValueDescriptorClient)
+func CoredataEventClientFrom(get di.Get) interfaces.EventClient {
+	return get(CoredataEventClientName).(interfaces.EventClient)
 }
--- internal/container/deviceservice.go
+++ internal/container/deviceservice.go
@@ -1,34 +1,38 @@
 // -*- Mode: Go; indent-tabs-mode: t -*-
 //
-// Copyright (C) 2020 IOTech Ltd
+// Copyright (C) 2020-2021 IOTech Ltd
 //
 // SPDX-License-Identifier: Apache-2.0
 
 package container
 
 import (
-	"github.com/edgexfoundry/device-sdk-go/v2/pkg/models"
 	"github.com/edgexfoundry/go-mod-bootstrap/v2/di"
-	contract "github.com/edgexfoundry/go-mod-core-contracts/v2/models"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/v2/models"
+
+	sdkModels "github.com/edgexfoundry/device-sdk-go/v2/pkg/models"
 )
 
 // DeviceServiceName contains the name of device service struct in the DIC.
-var DeviceServiceName = di.TypeInstanceToName(contract.DeviceService{})
+var DeviceServiceName = di.TypeInstanceToName(models.DeviceService{})
 
 // ProtocolDiscoveryName contains the name of protocol discovery implementation in the DIC.
-var ProtocolDiscoveryName = di.TypeInstanceToName((*models.ProtocolDiscovery)(nil))
+var ProtocolDiscoveryName = di.TypeInstanceToName((*sdkModels.ProtocolDiscovery)(nil))
 
 // ProtocolDriverName contains the name of protocol driver implementation in the DIC.
-var ProtocolDriverName = di.TypeInstanceToName((*models.ProtocolDriver)(nil))
+var ProtocolDriverName = di.TypeInstanceToName((*sdkModels.ProtocolDriver)(nil))
+
+// ManagerName contains the name of autoevent manager implementation in the DIC
+var ManagerName = di.TypeInstanceToName((*sdkModels.Manager)(nil))
 
 // DeviceServiceFrom helper function queries the DIC and returns device service struct.
-func DeviceServiceFrom(get di.Get) *contract.DeviceService {
-	return get(DeviceServiceName).(*contract.DeviceService)
+func DeviceServiceFrom(get di.Get) *models.DeviceService {
+	return get(DeviceServiceName).(*models.DeviceService)
 }
 
 // ProtocolDiscoveryFrom helper function queries the DIC and returns protocol discovery implementation.
-func ProtocolDiscoveryFrom(get di.Get) models.ProtocolDiscovery {
-	casted, ok := get(ProtocolDiscoveryName).(models.ProtocolDiscovery)
+func ProtocolDiscoveryFrom(get di.Get) sdkModels.ProtocolDiscovery {
+	casted, ok := get(ProtocolDiscoveryName).(sdkModels.ProtocolDiscovery)
 	if ok {
 		return casted
 	}
@@ -36,6 +40,11 @@ func ProtocolDiscoveryFrom(get di.Get) models.ProtocolDiscovery {
 }
 
 // ProtocolDriverFrom helper function queries the DIC and returns protocol driver implementation.
-func ProtocolDriverFrom(get di.Get) models.ProtocolDriver {
-	return get(ProtocolDriverName).(models.ProtocolDriver)
+func ProtocolDriverFrom(get di.Get) sdkModels.ProtocolDriver {
+	return get(ProtocolDriverName).(sdkModels.ProtocolDriver)
+}
+
+// ManagerFrom helper function queries the DIC and returns autoevent manager implementation
+func ManagerFrom(get di.Get) sdkModels.Manager {
+	return get(ManagerName).(sdkModels.Manager)
 }
