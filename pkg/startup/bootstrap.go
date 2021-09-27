931081e8f50191b50f15fecf55a8e6cf480936a0
edgego <xgtian@163.com>
2021-09-27 11:05:58 +0800

    add prometheus metrics

--- pkg/startup/bootstrap.go
+++ pkg/startup/bootstrap.go
@@ -12,9 +12,13 @@ import (
 
 	"github.com/edgexfoundry/device-sdk-go/v2/pkg/service"
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func Bootstrap(serviceName string, serviceVersion string, driver interface{}) {
 	ctx, cancel := context.WithCancel(context.Background())
+	//add promethues metrics，modified by jacktian
+	r := mux.NewRouter()
+	r.Handle("/metrics", promhttp.Handler())
 	service.Main(serviceName, serviceVersion, driver, ctx, cancel, mux.NewRouter())
 }
