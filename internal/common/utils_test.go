dc7f6bf09fc38c567ebe9ac7e79f329d8da024ff
Leonard Goodell <leonard.goodell@intel.com>
2022-10-17 12:18:01 -0700

    feat: Add metrics for count of Events and Readings sent
    closes #1238
    
    Signed-off-by: Leonard Goodell <leonard.goodell@intel.com>

--- internal/common/utils_test.go
+++ internal/common/utils_test.go
@@ -6,17 +6,21 @@
 package common
 
 import (
+	"errors"
 	"testing"
 
 	bootstrapContainer "github.com/edgexfoundry/go-mod-bootstrap/v2/bootstrap/container"
+	"github.com/edgexfoundry/go-mod-bootstrap/v2/bootstrap/interfaces/mocks"
 	"github.com/edgexfoundry/go-mod-bootstrap/v2/di"
 	clientMocks "github.com/edgexfoundry/go-mod-core-contracts/v2/clients/interfaces/mocks"
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/clients/logger"
+	mocks2 "github.com/edgexfoundry/go-mod-core-contracts/v2/clients/logger/mocks"
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/common"
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos"
 	dtoCommon "github.com/edgexfoundry/go-mod-core-contracts/v2/dtos/common"
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos/requests"
 	msgMocks "github.com/edgexfoundry/go-mod-messaging/v2/messaging/mocks"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
@@ -115,6 +119,100 @@ func TestSendEvent(t *testing.T) {
 				ecMock.AssertNumberOfCalls(t, "Add", 1)
 				mcMock.AssertNumberOfCalls(t, "Publish", 0)
 			}
+		})
+	}
+}
+
+func Test_HandleEventReadingMetrics(t *testing.T) {
+	event1Reading := dtos.NewEvent("Test", "Test", "Test")
+	err := event1Reading.AddSimpleReading("Test", common.ValueTypeInt32, int32(123))
+	require.NoError(t, err)
+	event3Readings := dtos.NewEvent("Test", "Test", "Test")
+	err = event3Readings.AddSimpleReading("Test1", common.ValueTypeInt32, int32(123))
+	require.NoError(t, err)
+	err = event3Readings.AddSimpleReading("Test2", common.ValueTypeInt32, int32(123))
+	require.NoError(t, err)
+	err = event3Readings.AddSimpleReading("Test3", common.ValueTypeInt32, int32(123))
+	require.NoError(t, err)
+
+	tests := []struct {
+		Name                    string
+		TimesToCall             int
+		Event                   dtos.Event
+		MetricsManagerAvailable bool
+		RegisterError           bool
+		ExpectedEventCount      int64
+		ExpectedReadingCount    int64
+	}{
+		{"First Pass", 1, event1Reading, true, false, 1, 1},
+		{"3 Passes with 1 reading", 3, event1Reading, true, false, 3, 3},
+		{"3 Passes with 3 readings", 3, event3Readings, true, false, 3, 9},
+		{"No MetricsManager", 1, event1Reading, false, false, 1, 1},
+		{"Error Registering", 1, event1Reading, true, true, 1, 1},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			eventsSent = nil
+			readingsSent = nil
+
+			mockManager := &mocks.MetricsManager{}
+			mockLogger := &mocks2.LoggingClient{}
+
+			if test.RegisterError {
+				mockManager.On("Register", mock.Anything, mock.Anything, mock.Anything).Return(errors.New("failed"))
+			} else {
+				mockManager.On("Register", mock.Anything, mock.Anything, mock.Anything).Return(nil)
+
+			}
+
+			mockLogger.On("Warn", mock.Anything).Return()
+			mockLogger.On("Debugf", mock.Anything, mock.Anything).Return()
+			mockLogger.On("Errorf", mock.Anything, mock.Anything, mock.Anything).Return()
+
+			dic := di.NewContainer(di.ServiceConstructorMap{
+				bootstrapContainer.LoggingClientInterfaceName: func(get di.Get) interface{} {
+					return mockLogger
+				},
+				bootstrapContainer.MetricsManagerInterfaceName: func(get di.Get) interface{} {
+					if test.MetricsManagerAvailable {
+						return mockManager
+					}
+
+					return nil
+				},
+			})
+
+			n := 0
+			for n < test.TimesToCall {
+				handleEventReadingMetrics(&test.Event, mockLogger, dic)
+				n++
+			}
+
+			if test.MetricsManagerAvailable {
+				mockManager.AssertNumberOfCalls(t, "Register", 2)
+				mockLogger.AssertNumberOfCalls(t, "Warn", 0)
+
+				if test.RegisterError {
+					mockLogger.AssertNumberOfCalls(t, "Errorf", 2)
+					mockLogger.AssertNumberOfCalls(t, "Debugf", 0)
+
+				} else {
+					mockLogger.AssertNumberOfCalls(t, "Errorf", 0)
+					mockLogger.AssertNumberOfCalls(t, "Debugf", 2)
+				}
+
+			} else {
+				mockManager.AssertNumberOfCalls(t, "Register", 0)
+				mockLogger.AssertNumberOfCalls(t, "Warn", 1)
+				mockLogger.AssertNumberOfCalls(t, "Debugf", 0)
+			}
+
+			require.NotNil(t, eventsSent)
+			require.NotNil(t, readingsSent)
+
+			assert.Equal(t, test.ExpectedEventCount, eventsSent.Count())
+			assert.Equal(t, test.ExpectedReadingCount, readingsSent.Count())
 		})
 	}
 }
