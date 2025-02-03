15245c0ccff1ea0539cab22d03c7239b5b366580
FelixTing <felix@iotechsys.com>
2025-02-03 09:58:46 +0800

    fix: Address change requests
    Signed-off-by: FelixTing <felix@iotechsys.com>

--- internal/autoevent/executor.go
+++ internal/autoevent/executor.go
@@ -53,13 +53,9 @@ func (e *Executor) Run(ctx context.Context, wg *sync.WaitGroup, buffer chan bool
 		select {
 		case <-ctx.Done():
 			return
-		default:
+		case <-time.After(time.Until(deadline)):
 			if e.stop {
 				return
-			}
-			sleepDuration := time.Until(deadline)
-			if sleepDuration > 0 {
-				time.Sleep(sleepDuration)
 			}
 			deadline = deadline.Add(e.duration)
 			lc.Debugf("AutoEvent - reading %s", e.sourceName)
